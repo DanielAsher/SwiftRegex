17901d7d014d90663b99902c44a1f365c60d7bfe
Daniel <daniel.asher.2303@gmail.com>
2015-04-27 06:27:00 +0100

    add SwiftRegexOSX target

---
+++ SwiftRegexOSX/SwiftRegexOSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftRegexOSX.h
+//  SwiftRegexOSX
+//
+//  Created by Daniel Asher on 27/04/2015.
+//  Copyright (c) 2015 John Holdsworth. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftRegexOSX.
+FOUNDATION_EXPORT double SwiftRegexOSXVersionNumber;
+
+//! Project version string for SwiftRegexOSX.
+FOUNDATION_EXPORT const unsigned char SwiftRegexOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftRegexOSX/PublicHeader.h>
+
+
